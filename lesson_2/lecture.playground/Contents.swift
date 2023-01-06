5532042814e7d5c8486ed602507d7641cf5bccac
Jano Voskanyan <jano@MacBook-Pro-Jano.local>
2023-01-06 13:37:58 +0400

    add lesson 2

---
+++ lesson_2/lecture.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//:Arithmetic operator
+
+var number1 = 9
+var number2 = 4
+
+number1 + number2
+number1 - number2
+number1 * number2
+number1 / number2
+
+//:Remainder Operator
+
+number1 % number2
+-number1 % number2
+number1 % -number2
+
+//:Assignment Operator
+
